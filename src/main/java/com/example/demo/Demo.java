77efe601047573b785498833e0a4c220de73d03d
abhishekdsangani <63462358+abhishekdsangani@users.noreply.github.com>
2023-09-30 14:29:45 +0530

    Public IP Listen on 172.178.99.200:8080

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Demo {
 
-    @GetMapping("/ping")
+    @GetMapping("")
     public String ping(){
-        return "ping";
+        return "<h1>Abhishek Sangani</h1>";
     }
 }
