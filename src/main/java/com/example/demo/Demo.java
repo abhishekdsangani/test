43ae623eba3ee434c5d7c4b590865fe54c0fba72
abhishekdsangani <63462358+abhishekdsangani@users.noreply.github.com>
2024-02-11 14:42:12 +0530

    testing squash git testing last

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -9,6 +9,7 @@ public class Demo {
     @GetMapping("/ping")
     public String ping(){
         System.out.println("true = " + true);
+        System.out.println("false= " + false);
         return "<h1>Abhishek Sangani</h1>";
     }
 }
